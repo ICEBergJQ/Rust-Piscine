3153a09cdeaf42751f22f1be565c83ba11f92012
ICEBergJQ <salahsk52@gmail.com>
2025-07-24 11:08:59 +0100

    fixed

--- scalar/target/debug/deps/scalar-f80e6bc916d06c96.d
+++
@@ -1,5 +0,0 @@
-/home/sakacmi/Desktop/piscine-rust/scalar/target/debug/deps/scalar-f80e6bc916d06c96.d: src/lib.rs
-
-/home/sakacmi/Desktop/piscine-rust/scalar/target/debug/deps/libscalar-f80e6bc916d06c96.rmeta: src/lib.rs
-
-src/lib.rs:
