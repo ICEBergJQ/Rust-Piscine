086bea09744afe4a770d2f1dbead1a02211e40bc
ICEBergJQ <salahsk52@gmail.com>
2025-07-24 15:22:37 +0100

    fixed groceries

--- groceries/src/lib.rs
+++ groceries/src/lib.rs
@@ -1,16 +1,7 @@
 pub fn insert(vec: &mut Vec<String>, val: String) {
+    vec.push(val);
 }
 
 pub fn at_index(slice: &[String], index: usize) -> &str {
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    &slice[index]
 }
