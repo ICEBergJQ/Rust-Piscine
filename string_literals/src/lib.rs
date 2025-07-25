685de481b8c5045dcd18975ecb76eafd88385f76
ICEBergJQ <salahsk52@gmail.com>
2025-07-25 13:01:16 +0100

    adding string literals

---
+++ string_literals/src/lib.rs
@@ -0,0 +1,26 @@
+pub fn is_empty(v: &str) -> bool {
+    v.is_empty()
+}
+
+pub fn is_ascii(v: &str) -> bool {
+    v.is_ascii()
+}
+
+pub fn contains(v: &str, pat: &str) -> bool {
+    v.contains(pat)
+}
+
+pub fn split_at(v: &str, index: usize) -> (&str, &str) {
+    v.split_at(index)
+}
+
+pub fn find(v: &str, pat: char) -> usize {
+    let mut ind: usize = 0;
+    for i in v.chars() {
+        if i == pat {
+            return ind;
+        }
+        ind += 1;
+    }
+    ind
+}
