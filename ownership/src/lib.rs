b485714217febff765ce2ac540562461dcb5922a
ICEBergJQ <salahsk52@gmail.com>
2025-07-26 09:53:21 +0100

    adding ownership

--- ownership/src/lib.rs
+++ ownership/src/lib.rs
@@ -1,3 +1,13 @@
 pub fn first_subword(mut s: String) -> String {
-    
+      for (i, c) in s.char_indices() {
+        if i == 0 {
+            continue;
+        }
+
+        if c == '_' || c.is_uppercase() {
+            s = s[..i].to_string();
+            break;
+        }
+    }
+    s
 }
