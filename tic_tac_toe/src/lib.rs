4b206041bbeff01ad36d3f9d291402d471e25a77
ICEBergJQ <salahsk52@gmail.com>
2025-07-26 17:18:32 +0100

    fixed

--- tic_tac_toe/src/lib.rs
+++ tic_tac_toe/src/lib.rs
@@ -8,7 +8,7 @@ pub fn tic_tac_toe(table: [[char; 3]; 3]) -> String {
     }
 
     for &player in &players {
-        if diagonals(player, &table) || horizontal(player, &table) || vertical(player, &table) {
+        if diagonals(player, table) || horizontal(player, table) || vertical(player, table) {
             return format!("player {} won", player);
         }
     }
@@ -16,16 +16,16 @@ pub fn tic_tac_toe(table: [[char; 3]; 3]) -> String {
     "tie".to_string()
 }
 
-pub fn diagonals(player: char, table: &[[char; 3]; 3]) -> bool {
+pub fn diagonals(player: char, table: [[char; 3]; 3]) -> bool {
     let main_diag = (0..3).all(|i| table[i][i] == player);
     let anti_diag = (0..3).all(|i| table[i][2 - i] == player);
     main_diag || anti_diag
 }
 
-pub fn horizontal(player: char, table: &[[char; 3]; 3]) -> bool {
+pub fn horizontal(player: char, table: [[char; 3]; 3]) -> bool {
     table.iter().any(|row| row.iter().all(|&c| c == player))
 }
 
-pub fn vertical(player: char, table: &[[char; 3]; 3]) -> bool {
+pub fn vertical(player: char, table: [[char; 3]; 3]) -> bool {
     (0..3).any(|col| (0..3).all(|row| table[row][col] == player))
 }
