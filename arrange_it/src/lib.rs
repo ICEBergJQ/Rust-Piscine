99d8ddf7f5c32f5ed760f816a13a72e49ec808dc
ICEBerg <ICE@L33HW4k.192.168.1.1>
2025-07-26 19:05:23 -0400

    fixed arrange_it

--- arrange_it/src/lib.rs
+++ arrange_it/src/lib.rs
@@ -1,18 +1,30 @@
 pub fn arrange_phrase(phrase: &str) -> String {
-    let mut _temp: Vec<(i32, &str)> = Vec::new();
+    let mut _temp: Vec<(i32, String)> = Vec::new();
+    let mut res = String::new();
 
     for word in phrase.split_whitespace() {
+        let mut _wrd = String::new();
         let mut number_str = String::new();
+
         for c in word.chars() {
             if c.is_numeric() {
                 number_str.push(c);
+            } else {
+                _wrd.push(c);
             }
         }
-        if !number_str.is_empty() {
-            if let Ok(num) = number_str.parse::<i32>() {
-                _temp.push((num, word));
-            }
+
+        if let Ok(num) = number_str.parse::<i32>() {
+            _temp.push((num, _wrd));
         }
     }
+
     _temp.sort_by_key(|k| k.0);
+
+    for (_, wrd) in &_temp {
+        res.push_str(wrd);
+        res.push(' ');
+    }
+    res.pop();
+    res
 }
