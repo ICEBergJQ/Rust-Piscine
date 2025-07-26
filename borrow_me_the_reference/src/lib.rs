0b4624e9522a9c2355f53cafc3d012173f9e19cb
ICEBergJQ <salahsk52@gmail.com>
2025-07-26 14:37:30 +0100

    fixed

--- borrow_me_the_reference/src/lib.rs
+++ borrow_me_the_reference/src/lib.rs
@@ -1,43 +1,22 @@
 pub fn delete_and_backspace(s: &mut String) {
-    let mut back_s: bool = false;
     let mut op_c: usize = 0;
     let mut temp = String::new();
-    let clone = s.clone();
-    let mut next_ind: usize = 0;
-    for (i, c) in clone.char_indices() {
-        if i < next_ind {
+    for c in s.chars() {
+        if c == '-' {
+            temp.pop();
+            continue;
+        } else if c == '+' {
+            op_c += 1;
             continue;
         }
-        if c == '-' || c == '+' {
-            op_c = op_count(i, &clone, c);
-            next_ind = i + op_c;
-            if c == '-' {
-                back_s = true;
-            } else {
-                next_ind += op_c;
-            }
+        if op_c > 0 {
+            op_c -= 1;
+            continue;
         } else {
             temp.push(c);
         }
-        if back_s {
-            temp.truncate(temp.len() - op_c);
-            back_s = false;
-        }
     }
     *s = temp;
-}
-
-fn op_count(ind: usize, s: &str, op: char) -> usize {
-    let mut count = 0;
-    for c in s[ind..].chars() {
-        if c == op {
-            count += 1;
-        } else {
-            break;
-        }
-    }
-
-    count
 }
 
 pub fn do_operations(v: &mut [String]) {
