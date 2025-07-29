5271fc0d89c1bea19e2d56908be08c6ed65fd29b
ICEBergJQ <salahsk52@gmail.com>
2025-07-29 08:51:27 +0100

    fixed

--- string_permutation/src/lib.rs
+++ string_permutation/src/lib.rs
@@ -1,18 +1,25 @@
+use std::collections::HashMap;
+
 pub fn is_permutation(s1: &str, s2: &str) -> bool {
     if s1.len() != s2.len() {
         return false;
     }
 
-    let mut char_count = [0; 256];
+    let mut count_map = HashMap::new();
 
     for c in s1.chars() {
-        char_count[c as usize] += 1;
+        *count_map.entry(c).or_insert(0) += 1;
     }
 
     for c in s2.chars() {
-        char_count[c as usize] -= 1;
-        if char_count[c as usize] < 0 {
-            return false;
+        match count_map.get_mut(&c) {
+            Some(count) => {
+                if *count == 0 {
+                    return false;
+                }
+                *count -= 1;
+            }
+            None => return false,
         }
     }
 
