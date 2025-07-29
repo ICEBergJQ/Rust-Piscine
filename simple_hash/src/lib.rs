b9f274ba1c5cea70c61d8ba14af4b2d818e27550
ICEBergJQ <salahsk52@gmail.com>
2025-07-29 08:41:42 +0100

    fixed

--- simple_hash/src/lib.rs
+++ simple_hash/src/lib.rs
@@ -1,5 +1,5 @@
-pub fn word_frequency_counter<'a>(words: &[&'a str]) -> HashMap<&'a str, usize> {
 use std::collections::HashMap;
+pub fn word_frequency_counter<'a>(words: &[&'a str]) -> HashMap<&'a str, usize> {
     let mut frequency_count = HashMap::new();
     for &word in words {
         *frequency_count.entry(word).or_insert(0) += 1;
