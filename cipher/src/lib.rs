531479539d4900f5c6d33a299e69b8228bd9f2ff
ICEBergJQ <salahsk52@gmail.com>
2025-08-01 17:42:58 +0100

    adding cipher

--- cipher/src/lib.rs
+++ cipher/src/lib.rs
@@ -5,23 +5,22 @@ pub struct CipherError {
 
 pub fn cipher(original: &str, ciphered: &str) -> Result<(), CipherError> {
     let mut lmodir = String::new();
-    for i in  original.chars() {
+    for i in  original.bytes() {
             if i.is_ascii_alphabetic() {
-                let base = if i.is_lowercase() { 'a' } else { 'A' };
-                let base2 = if i.is_lowercase() { 'z' } else { 'Z' };
-                let mirrored =(base as u8 + (base2 as u8 - i as u8)) as char;
-                lmodir.push(mirrored);
+                if i.is_ascii_lowercase() {                    
+                    lmodir.push((122 - (i - 97)) as char);
+                } else if i.is_ascii_uppercase() {
+                    lmodir.push((90 - (i - 65)) as char);
+                }
             } else {
-                lmodir.push(i);
-        }
+                lmodir.push(i as char);
+            }
     }
     if lmodir == ciphered {
-        Ok(())
-    } else {
-        Err(CipherError {
-            expected: lmodir,
-        })
+        return Ok(());
     }
+    Err(CipherError {
+        expected: lmodir,
+    })
+    
 }
-
-
