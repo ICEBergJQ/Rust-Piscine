91bac230b26b10fcdd1800c2abe035b7313d0ffc
ICEBergJQ <salahsk52@gmail.com>
2025-08-01 12:14:45 +0100

    fixed

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -1,21 +1,13 @@
 use std::path::Path;
 use std::fs::{OpenOptions};
 use std::io::Write;
-use std::fs::File;
 
 pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
-
-    let path_ref = path.as_ref();
-
-    if !path_ref.exists() {
-        let mut file = OpenOptions::new()
-            .write(true)
-            .create(true)
-            .open(path_ref)
-            .expect("Failed to create file");
-        file.write_all(content.as_bytes())
-            .expect("Failed to write to file");
-    } else {
-        File::open(path_ref).unwrap();
-    }
+    let mut file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(path)
+        .unwrap();
+    
+    file.write_all(content.as_bytes()).unwrap();
 }
