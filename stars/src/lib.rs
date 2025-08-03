decfe975b78264a50893eeb7a2160529dd1db5f7
ICEBergJQ <salahsk52@gmail.com>
2025-08-03 16:21:47 +0100

    adding scores

--- stars/src/lib.rs
+++ stars/src/lib.rs
@@ -1,6 +1,6 @@
 pub fn stars(n: u32) -> String {
     let mut result = String::new();  
-    for i in 0..2_usize.pow(n) {
+    for _ in 0..2_usize.pow(n) {
         result.push('*');
     }
     result
